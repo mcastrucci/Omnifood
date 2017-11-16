46a0bdb329e188383542201aaf09f5f0d9816caf
Maximiliano Castrucci <Maximiliano.Castrucci@igt.com>
2017-11-16 18:05:23 -0300

    Added Omnifood webPage initial draft

---
+++ resources/js/scripts.js
@@ -0,0 +1,100 @@
+/*Stricky navigation*/
+$(document).ready(function (){
+    $('.--js-section-features').waypoint(function(direction){
+        if(direction == "down"){
+            $('nav').addClass('sticky');
+        }else{
+            $('nav').removeClass('sticky');
+        }
+    },{
+        offset: '60px;'
+    });
+    
+/*Scroll buttons*/
+    
+    $('.--js-scroll-to-plans').click(function (){
+        $('html, body').animate({scrollTop: $('.--js-section-plans').offset().top}, 1000);
+    });    
+    
+    $('.--js-scroll-to-start').click(function (){
+        $('html, body').animate({scrollTop: $('.--js-section-features').offset().top}, 1000);
+    });
+    
+    
+    /*---------------------------
+    -------Animations on scroll--*/
+    $('.--js-wp-1').waypoint(function(direction){
+        $('.--js-wp-1').addClass('animated fadeIn');
+    },{
+        offset: '50%'
+    });
+    
+    $('.--js-wp-2').waypoint(function(direction){
+        $('.--js-wp-2').addClass('animated bounceInUp');
+    },{
+        offset: '50%'
+    });    
+    
+    $('.--js-wp-3').waypoint(function(direction){
+        $('.--js-wp-3').addClass('animated pulse');
+    },{
+        offset: '70%'
+    });
+    
+    /*Mobile navigations*/
+    
+    $('.mobile-nav-icon').click(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        if(icon.hasClass('ion-navicon')){
+            icon.addClass('ion-close');
+            icon.removeClass('ion-navicon');
+        }else{
+            icon.addClass('ion-navicon');
+            icon.removeClass('ion-close');
+        }
+    })
+    
+    
+    /*-----------------------
+    -------Smoth scrolling--
+    */
+    // Select all links with hashes
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          }
+        }
+      });
+});
